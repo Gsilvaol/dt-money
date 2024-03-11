a68a9dba3400713e59ef8b1a2db5c2350ca37b8e
Gustavo Silva de Oliveira <gustavosilva1994_@hotmail.com>
2024-03-11 14:54:44 -0300

    tabela de transaçoes

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,44 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { PriceHiglight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
     return (
         <div>
             <Header />
             <Summary />
-        </div>
+
+            <TransactionsContainer>
+                <TransactionsTable>
+                    <tbody>
+                        <tr>
+                            <td width="50%">Desenvolvimento de site</td>
+                            <td>
+                                <PriceHiglight variant="income">
+                                    R$ 12.000,00
+                                </PriceHiglight>
+                            </td>
+                            <td>Venda</td>
+                            <td>11/03/2024</td>
+
+                        </tr>
+
+                        <tr>
+                            <td width="50%">Hamburguer</td>
+                            <td>
+                                <PriceHiglight variant="outcome">
+                                    -R$ 59,00
+                                </PriceHiglight>
+                            </td>
+                            <td>Venda</td>
+                            <td>11/03/2024</td>
+
+                        </tr>
+
+                    </tbody>
+                </TransactionsTable>
+            </TransactionsContainer>
+        
+        </div >
     )
 }
